51df2582f6e9585f235e6627be20b5d1fc5b7465
Hanasolo <hanasolo@hotmail.com>
2024-11-04 19:52:59 +0100

    starting html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <header>
+        <h1>ANÁS <span className="lastName">Elvira Fernandez</span></h1>
+        <h3>Frontent Developer</h3>
+      </header>
+      <main>
+        <section>
+          <h5>
+            "Decidí comenzar en el desarrollo web para explorar y entender los
+            fundamentos que construyen el ecosistema digital.
+            <br />
+            El camino ya ha empezado, y este será el sitio donde mi aprendizaje
+            se hará visible.
+            <br />
+            Aquí encontrarás no solo un portafolio, sino una crónica de los
+            trabajos que he completado, una referencia para seguir avanzando con
+            determinación. "
+          </h5>
+        </section>
+        <section>
+        section left  estas partes con grid y media para pasar a uno o dos 
+          <p>componente box donde aparece una imagen con enlace del proyecto</p>
+        </section>
+        <section>
+        section right
+        <p>componente box donde aparece una imagen con enlace del proyecto</p>
+        </section>
 
-export default App
+      </main>
+      <footer>redes y deja tu comentario un form post</footer>
+    </div>
+  );
+}
