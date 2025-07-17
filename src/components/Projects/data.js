a6ca0a09d728507625cdb88ba5dbd6089d7e32fc
Hanasolo <hanasolo@hotmail.com>
2025-07-17 19:04:34 +0200

    New image added

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -23,25 +23,28 @@
     },
     {   id : 4, 
         name : 'Note-App',
+        image : ["/Notes_01.png, Notes_02.png"],
         link : "https://note-app-24.netlify.app",
         date : '15.11.24',
         description : 'A \'complex\' app to lear how to manage and save data'
     },
     {   id : 5, 
         name : 'Travel Journal',
-        image : ["/base-travel-journal.png"],
+        image : "/base-travel-journal.png",
         link : "https://anas-travel-journal.netlify.app",
         date : '15.11.24',
         description : 'Components reusability, another basic exercise'
     },
     {   id : 6, 
         name : 'Tenzies',
+        image : "/Tenzies.png",
         link : "https://anas-tenzies-2024.netlify.app",
         date : '19.11.24',
         description : 'A basic React game, but not too basic'
     },
     {   id : 7, 
         name : 'Scoreboard',
+        image : "/scoreboard.png",
         link : "https://anas-scoreboard.netlify.app",
         date : '12.12.24',
         description : 'JavaScript Basics'
@@ -52,19 +55,20 @@
         date : '21.12.24',
         description : 'Api solo project, that takes spotify info and create new lists'
     },
-    {   id : 9, 
+   /* {   id : 9, 
         name : 'Van Life',
         link : "https://anas-van-life.netlify.app",
         date : '21.12.24',
         description : 'Avanced React with React Router'
-    },
+    },*/
     {   id : 10, 
         name : 'Password Gen',
+        image : "/Password_Gen.png",
         link : "https://anas-password-gen.app",
         date : '19.01.24',
         description : 'Basic Js deploiment'
     },
-    {   id : 11, 
+    /*{   id : 11, 
         name : 'Crome Extension',
         link : "",
         date : '02.02.25',
@@ -75,7 +79,7 @@
         link : "",
         date : '03.02.25',
         description : 'Js deploiment'
-    },
+    },*/
 
 ]
 const datas = data.sort((a, b) => new Date(a.date) - new Date(b.date))
